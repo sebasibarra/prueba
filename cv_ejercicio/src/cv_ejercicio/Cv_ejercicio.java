5469848a501e9d5b8e7fc98d3b6a21b8c7e091c9
Sebastian <Sebastian@DESKTOP-65ANSQM>
2020-06-01 10:52:36 -0500

    VERSION Sebastian PROYECTO

--- cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
+++ cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
@@ -17,6 +17,7 @@ public class Cv_ejercicio {
     public static void main(String[] args) {
         System.out.println("Version 0");
         System.out.println("version 1");
+        System.out.println("Version Sebastian");
     }
     
 }
