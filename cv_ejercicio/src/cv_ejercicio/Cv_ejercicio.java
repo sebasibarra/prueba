028d052f522caa64a18906c8b294ca4ab21feaf1
Sebastian <Sebastian@DESKTOP-65ANSQM>
2020-06-15 11:52:57 -0500

    Version desarrollador1

--- cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
+++ cv_ejercicio/src/cv_ejercicio/Cv_ejercicio.java
@@ -16,8 +16,7 @@ public class Cv_ejercicio {
      */
     public static void main(String[] args) {
         System.out.println("Version 0");
-        System.out.println("version 1");
-        System.out.println("Version Sebastian");
+        System.out.println("Desarrollador 11");
     }
-    
+  
 }
